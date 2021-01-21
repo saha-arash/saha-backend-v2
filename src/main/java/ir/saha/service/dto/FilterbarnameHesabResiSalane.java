cee8ae70112046c0be9a05bc82cf5550d473e57e
arshamsedaghatbin <arsham.sedaghatbin@gmail.com>
2021-01-21 20:10:34 +0330

    query on barname salane

---
+++ src/main/java/ir/saha/service/dto/FilterbarnameHesabResiSalane.java
@@ -0,0 +1,24 @@
+package ir.saha.service.dto;
+
+public class FilterbarnameHesabResiSalane {
+
+    private Integer saleshoroo;
+    private Integer salapayan;
+
+
+    public Integer getSaleshoroo() {
+        return saleshoroo;
+    }
+
+    public void setSaleshoroo(Integer saleshoroo) {
+        this.saleshoroo = saleshoroo;
+    }
+
+    public Integer getSalapayan() {
+        return salapayan;
+    }
+
+    public void setSalapayan(Integer salapayan) {
+        this.salapayan = salapayan;
+    }
+}
